209c170c0c7ff02417032f671459d5aba88bf601
wangkegang <wangkegang@51yche.com>
2015-07-17 16:54:18 +0800

    修改Bug

--- crawlers.py
+++ crawlers.py
@@ -15,15 +15,16 @@ def getHtml(url):
     print 'getHtml'
     req_header = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
     req_timeout = 20
+    content = ""
     try:
         req = urllib2.Request(url,None,req_header)
         resp = urllib2.urlopen(req,None,req_timeout)
-        baseHtml = resp.read()
+        content = resp.read()
     except urllib2.URLError as e:
         print e.message
     except socket.timeout as e:
         print e.message
-    return baseHtml
+    return content
 
 def getUrlNum(html):
     reg = r'<span class=\"current-comment-page\">\[(.+?)\]</span>'
@@ -33,9 +34,9 @@ def getUrlNum(html):
 
 def getImglistUrl(html):
     print 'getImglistUrl'
-    reg = r'src="(.+?\.jpg|.+?\.png|.+?\.gif)"'
+    reg = r'src="(.+?\.jpg|.+?\.png|.+?\.gif|.+?\.jpeg)"'
     image = re.compile(reg)
-    imglist = set(re.findall(image,html))
+    imglist = set(re.findall(image,html)).remove('http://s.jandan.com/static/img/chart.png')
     return imglist
 
 def saveImage(imglist,path,imgNum):
@@ -47,12 +48,12 @@ def saveImage(imglist,path,imgNum):
 
 print 'begin'
 imgNum = 0
-pageNum = input("请输入下载页数:".decode('utf-8').encode('gbk'))
+pageNum = input('请输入下载页数:'.decode('utf-8').encode('gbk'))
 path = getDocment()
-baseHtml = getHtml("http://jandan.net/pic")
-num = int(getUrlNum(baseHtml))
+baseHtml = getHtml('http://jandan.net/ooxx')
+num = int(getUrlNum(baseHtml))#仅仅是获得第一页的页码
 while pageNum > 0:
-    url = 'http://jandan.net/pic/page-' + str(num) + '#comments'
+    url = 'http://jandan.net/ooxx/page-' + str(num) + '#comments'
     print url
     html = getHtml(url)
     imglist = getImglistUrl(html)
